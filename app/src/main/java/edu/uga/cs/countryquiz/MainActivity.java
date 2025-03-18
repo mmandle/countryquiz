9b76a88da1cbca8dee6663ed8d05ab149b7518a9
wcepress4 <wcepress@yahoo.com>
2025-03-18 15:34:13 -0400

    Added comment and import

--- app/src/main/java/edu/uga/cs/countryquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/countryquiz/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.List;
+//Tony is chopped
 
 public class MainActivity extends AppCompatActivity {
  //trial
