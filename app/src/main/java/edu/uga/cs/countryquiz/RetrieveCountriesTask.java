d0bf9d7a0b81f0b988d806469915667e7fd4e01b
wcepress4 <wcepress@yahoo.com>
2025-03-25 12:21:11 -0400

    Implemented AsyncTask class

--- app/src/main/java/edu/uga/cs/countryquiz/RetrieveCountriesTask.java
+++ app/src/main/java/edu/uga/cs/countryquiz/RetrieveCountriesTask.java
@@ -1,4 +1,33 @@
 package edu.uga.cs.countryquiz;
 
-public class RetrieveCountriesTask {
+import android.os.AsyncTask;
+import java.util.List;
+
+public class RetrieveCountriesTask extends AsyncTask<Void, Void, List<String>> {
+    private CountryQuizData dbHelper;
+    private OnCountriesRetrievedListener listener;
+
+    public RetrieveCountriesTask(CountryQuizData dbHelper, OnCountriesRetrievedListener listener) {
+        this.dbHelper = dbHelper;
+        this.listener = listener;
+    }
+
+    @Override
+    protected List<String> doInBackground(Void... voids) {
+        dbHelper.open();
+        List<String> countries = dbHelper.getAllCountries();
+        dbHelper.close();
+        return countries;
+    }
+
+    @Override
+    protected void onPostExecute(List<String> countries) {
+        if (listener != null) {
+            listener.onCountriesRetrieved(countries);
+        }
+    }
+
+    public interface OnCountriesRetrievedListener {
+        void onCountriesRetrieved(List<String> countries);
+    }
 }
