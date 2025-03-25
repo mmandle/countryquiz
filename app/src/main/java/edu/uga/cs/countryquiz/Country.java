ade0473839ceb6bf8a9f49bd81e58faf19c94774
wcepress4 <wcepress@yahoo.com>
2025-03-25 12:19:34 -0400

    Implemented POJO classes

--- app/src/main/java/edu/uga/cs/countryquiz/Country.java
+++ app/src/main/java/edu/uga/cs/countryquiz/Country.java
@@ -1,4 +1,39 @@
 package edu.uga.cs.countryquiz;
 
 public class Country {
+    private long id;
+    private String name;
+    private String continent;
+
+    public Country() {
+    }
+
+    public Country(String name, String continent) {
+        this.name = name;
+        this.continent = continent;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
 }
--- app/src/main/java/edu/uga/cs/countryquiz/Quiz.java
+++ app/src/main/java/edu/uga/cs/countryquiz/Quiz.java
@@ -1,4 +1,41 @@
 package edu.uga.cs.countryquiz;
 
+import java.util.Date;
+
 public class Quiz {
+    private long id;
+    private String date;
+    private int score;
+
+    public Quiz() {
+    }
+
+    public Quiz(String date, int score) {
+        this.date = date;
+        this.score = score;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
