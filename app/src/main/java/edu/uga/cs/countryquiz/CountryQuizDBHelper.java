22a5fe15b85a529638c61f55365e05d52b7d8372
wcepress4 <wcepress@yahoo.com>
2025-03-25 12:10:10 -0400

    Added DB Helper class

---
+++ app/src/main/java/edu/uga/cs/countryquiz/CountryQuizDBHelper.java
@@ -0,0 +1,61 @@
+package edu.uga.cs.countryquiz;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+public class CountryQuizDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "countryquiz.db";
+    private static final int DATABASE_VERSION = 1;
+    private static CountryQuizDBHelper instance;
+
+    // Table names
+    public static final String TABLE_COUNTRIES = "countries";
+    public static final String TABLE_QUIZZES = "quizzes";
+
+    // Country table columns
+    public static final String COLUMN_COUNTRY_ID = "id";
+    public static final String COLUMN_COUNTRY_NAME = "name";
+    public static final String COLUMN_COUNTRY_CONTINENT = "continent";
+
+    // Quiz table columns
+    public static final String COLUMN_QUIZ_ID = "id";
+    public static final String COLUMN_QUIZ_DATE = "date";
+    public static final String COLUMN_QUIZ_SCORE = "score";
+
+    // SQL statements
+    private static final String CREATE_COUNTRIES_TABLE = "CREATE TABLE " + TABLE_COUNTRIES + " ("
+            + COLUMN_COUNTRY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_COUNTRY_NAME + " TEXT NOT NULL, "
+            + COLUMN_COUNTRY_CONTINENT + " TEXT NOT NULL);";
+
+    private static final String CREATE_QUIZZES_TABLE = "CREATE TABLE " + TABLE_QUIZZES + " ("
+            + COLUMN_QUIZ_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_QUIZ_DATE + " TEXT NOT NULL, "
+            + COLUMN_QUIZ_SCORE + " INTEGER NOT NULL);";
+
+    private CountryQuizDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static synchronized CountryQuizDBHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new CountryQuizDBHelper(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_COUNTRIES_TABLE);
+        db.execSQL(CREATE_QUIZZES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COUNTRIES);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_QUIZZES);
+        onCreate(db);
+    }
+}
