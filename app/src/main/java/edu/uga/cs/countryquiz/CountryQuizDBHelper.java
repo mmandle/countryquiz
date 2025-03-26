7204f430364be51d4ba165a81bdbc552f59097ac
wcepress4 <wcepress@yahoo.com>
2025-03-25 21:52:54 -0400

    Removed unnecessary imports from DB helper

--- app/src/main/java/edu/uga/cs/countryquiz/CountryQuizDBHelper.java
+++ app/src/main/java/edu/uga/cs/countryquiz/CountryQuizDBHelper.java
@@ -1,11 +1,8 @@
 package edu.uga.cs.countryquiz;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CountryQuizDBHelper extends SQLiteOpenHelper {
 
