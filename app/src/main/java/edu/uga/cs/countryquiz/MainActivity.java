fc47aaea973499b209854d43ed4f288d5caebe82
wcepress4 <wcepress@yahoo.com>
2025-04-01 12:02:24 -0400

    Made changes to main activity

--- app/src/main/java/edu/uga/cs/countryquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/countryquiz/MainActivity.java
@@ -4,16 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements CSVReaderTask.OnCSVReadListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String DEBUG_TAG = "MainActivity";
 
@@ -29,46 +27,34 @@ public class MainActivity extends AppCompatActivity implements CSVReaderTask.OnC
             return insets;
         });
 
-        // Initialize buttons
         Button startQuizButton = findViewById(R.id.newQuiz);
         Button viewResultsButton = findViewById(R.id.pastResults);
 
-        // Find the "New Quiz" button
         startQuizButton.setOnClickListener(v -> startQuiz());
         viewResultsButton.setOnClickListener(v -> {});
 
-        // Start reading the CSV file asynchronously
-        new CSVReaderTask(this, this).execute();
+        // Check if the database already has countries before reading CSV
+        CountryQuizData countryQuizData = new CountryQuizData(this);
+        new RetrieveCountries(countryQuizData, countries -> {
+            if (countries.isEmpty()) {
+                Log.d(DEBUG_TAG, "Database is empty. Reading CSV and storing countries...");
+                new CSVReaderTask(this, countriesList -> {
+                    if (countriesList != null && !countriesList.isEmpty()) {
+                        Log.d(DEBUG_TAG, "Storing countries into database...");
+                        new StoreCountries(new CountryQuizData(this)).execute(countriesList);
+                    } else {
+                        Log.e(DEBUG_TAG, "No countries to store.");
+                    }
+                }).execute();
+            } else {
+                Log.d(DEBUG_TAG, "Database already contains " + countries.size() + " countries. Skipping CSV read.");
+            }
+        }).execute();
     }
 
     private void startQuiz() {
-        // Logic to start the quiz (start a new activity, for example)
         Log.d(DEBUG_TAG, "Starting quiz...");
         Intent intent = new Intent(MainActivity.this, QuizActivity.class);
         startActivity(intent);
-    }
-
-//    private void viewResults() {
-//        // Logic to view results (start a new activity for the results)
-//        Log.d(DEBUG_TAG, "Viewing results...");
-//        Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
-//        startActivity(intent);
-//    }
-
-    @Override
-    public void onCSVRead(List<Country> countries) {
-        // Callback method when CSV is read successfully
-        if (countries != null && !countries.isEmpty()) {
-            Log.d(DEBUG_TAG, "Inserting countries into database...");
-
-            // Create an instance of CountryQuizData to interact with the database
-            CountryQuizData countryQuizData = new CountryQuizData(this);
-
-            // Use the InsertCountries class to insert countries asynchronously
-            new InsertCountries(countryQuizData).execute(countries);
-
-        } else {
-            Log.e(DEBUG_TAG, "No countries to insert.");
-        }
     }
 }
